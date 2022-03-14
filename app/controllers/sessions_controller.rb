583cb7a6c44d2c282ee463704d7593aad001f068
alyonasdlr <elena.avdeeva@kellogg.northwestern.edu>
2022-03-14 19:56:05 +0000

    save 1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,23 @@ class SessionsController < ApplicationController
   end
 
   def create
+    entered_email = params["email"]
+    entered_password = params["password"]
+    @user = User.find_by({email: entered_email})
+    if @user 
+      if BCrypt::Password.new(@user.password) == entered_password
+        session["user_id"] = @user.id
+        flash[:notice] = "Welcome!"
+        redirect_to "/places"
+      else  
+      flash[:notice] = "Password is incorrect"
+      redirect_to "/sessions/new"  
+      end 
+    else
+      flash[:notice] = "No user with that email address"
+      redirect_to "/sessions/new"
+    end
+
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,21 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
+    def new
+      @user = User.new
+    end
+
+    def create
+      plain_text_password = params["user"]["password"]
+      @user = User.new(params["user"])
+      @user.password = BCrypt::Password.create(plain_text_password)
+      @user.save
+      redirect_to "/users/#{@user.id}"
+    end
+
+
+
+  def show
+    @user = User.find(params["id"])
   end
 
-  def create
-    @user = User.new(params["user"])
-    @user.save
-    redirect_to "/"
-  end
+
 end
